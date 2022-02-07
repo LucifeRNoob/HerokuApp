2c0b22fa91066ba2b1977c4b3f366133138bce7c
LucifeRNoob <andrewkremenevskiy@gmail.com>
2022-02-07 16:09:52 +0300

    Initial commit

---
+++ src/test/java/staticdata/WebUrls.java
@@ -0,0 +1,9 @@
+package staticdata;
+
+public class WebUrls {
+
+    public static final String HEROKU_APP_ELEMENTS_URL = "http://the-internet.herokuapp.com/add_remove_elements/";
+    public static final String HEROKU_APP_TABLES_URL = "http://the-internet.herokuapp.com/tables";
+
+
+}
