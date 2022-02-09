9c4bbccdc3bba172ee8da76b903def761ddce167
LucifeRNoob <andrewkremenevskiy@gmail.com>
2022-02-09 20:29:03 +0300

    HerokuApp Tests

--- src/test/java/staticdata/WebUrls.java
+++ src/test/java/staticdata/WebUrls.java
@@ -4,6 +4,10 @@ public class WebUrls {
 
     public static final String HEROKU_APP_ELEMENTS_URL = "http://the-internet.herokuapp.com/add_remove_elements/";
     public static final String HEROKU_APP_TABLES_URL = "http://the-internet.herokuapp.com/tables";
+    public static final String HEROKU_APP_CHECKBOX_URL = "http://the-internet.herokuapp.com/checkboxes";
+    public static final String HEROKU_APP_INPUTS_URL = "http://the-internet.herokuapp.com/inputs";
+    public static final String HEROKU_APP_TYPOS_URL = "http://the-internet.herokuapp.com/typos";
+    public static final String HEROKU_APP_NOTIFICATION_MESSAGES_URL = "http://the-internet.herokuapp.com/notification_message_rendered";
 
 
 }
